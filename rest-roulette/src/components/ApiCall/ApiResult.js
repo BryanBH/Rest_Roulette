474ae44b89b46a7e0fdfd0ea75bc3b278fc1abd8
BryanBH <47891748+BryanBH@users.noreply.github.com>
2022-03-28 11:10:56 -0400

    API functionallity working and displaying on result page

--- rest-roulette/src/components/ApiCall/ApiResult.js
+++ rest-roulette/src/components/ApiCall/ApiResult.js
@@ -4,36 +4,60 @@ import { finalCusine } from "../../pages/Roulette";
 
 const ApiResult = () => {
 	const [businesses, setBusinesses] = useState();
-	// const variables = {
-	//     category: finalCusine,
-	//     location: 'boston'
-	// }
-	useEffect(() => {
-		// axios.post("/sendVariables", variables)
-		//     .then(res => {
-		//     console.log(res)
-		// })
 
-        // call to local server 
-		axios.get("/getYelpAPI").then((response) => {
-			const responseBusinesses = response.data.object;
-            setBusinesses(responseBusinesses);
-            // console.log(businesses)
-		});
-	},[]);
+	var options = {
+		method: "GET",
+		url: `https://api.yelp.com/v3/businesses/search?categories=${finalCusine}&location=boston&limit=10`,
+		headers: {
+			Authorization:
+				"Bearer e1wKGcbUYGY-WsYjSvhbF3R0my11_JefuLR3z-W4gdGf2PZYJdvD2GEwolroAdtlDoTX7TIoAscQbDwiS3v02h1k3lYAJefop2mmi_7CSgSRhY2N7D20eDx6vss4YnYx",
+		},
+	};
+	useEffect(() => {
+		// axios
+		// 	.post("/variables", {
+		// 		category: finalCusine,
+		// 		area: "boston"
+		// 	})
+		// 	.then((res) => {
+		// 		console.log(res.status);
+		// 	})
+		// 	.catch((err) => {
+		// 		console.log("Error!", err);
+		// 	});
+
+		// call to local server
+		axios
+			.get("/getYelpAPI", {
+				params: {
+					categories: finalCusine,
+					location: "boston",
+				},
+			})
+			.then((response) => {
+				const responseBusinesses = response.data.object;
+				setBusinesses(responseBusinesses);
+			});
+	}, []);
 
 	return (
 		<>
-            <h1> API call results</h1>
-            {/* checks if business variable is not undefined, if it isn't then map and display the name and rating */}
-            {businesses && businesses.map((business => {
-                const { name, rating } = business;
-                return (
-                    <>
-                        <h3>Business name: {name}, Rating: {rating}</h3>
-                    </>
-                )
-            }))}
+			<h1> API call results</h1>
+			{/* checks if business variable is not undefined, if it isn't then map and display the name and rating */}
+			{businesses &&
+				businesses.map((business) => {
+					const { name, rating, phoneNumber } = business;
+					return (
+						<>
+							<p>
+								<h3>
+									Business name: {name}, Rating: {rating}
+								</h3>
+								<h4>Phone Number: { phoneNumber}</h4>
+							</p>
+						</>
+					);
+				})}
 		</>
 	);
 };
