f9eefb914b68fb4c672e140f0ae940d4a4916ace
BryanBH <47891748+BryanBH@users.noreply.github.com>
2022-04-03 22:42:17 -0400

    Added remove cuisine type from wheel functionallity, set structure for add functionallity

--- rest-roulette/src/pages/Roulette.js
+++ rest-roulette/src/pages/Roulette.js
@@ -2,15 +2,21 @@ import React, { useEffect, useState } from "react";
 import { Wheel } from "react-custom-roulette";
 import { NavBtn, NavBtnLink } from "../components/Navbar/NavbarElement";
 import useGeolocation from "../hooks/useGeolocation";
+import Select from "react-select";
+
+/**
+ * Initial cuisine type data
+ */
 const data = [
-	{ option: "chinese" },
-	{ option: "mexican" },
-	{ option: "italian" },
-	{ option: "korean" },
-	{ option: "greek" },
-	{ option: "spanish" },
-	{ option: "moroccan" },
-	{ option: "japanese" },
+	{ option: "chinese", label: "Chinese" },
+	{ option: "mexican", label: "Mexican" },
+	{ option: "italian", label: "Italian" },
+	{ option: "korean", label: "Korean" },
+	{ option: "greek", label: "Greek" },
+	{ option: "spanish", label: "Spanish" },
+	{ option: "moroccan", label: "Moroccan" },
+	{ option: "japanese", label: "Japanese" },
+	{ option: "colombian", label: "Colombian" },
 ];
 
 let finalCusine = "";
@@ -21,13 +27,22 @@ const Roulette = () => {
 	const [mustSpin, setMustSpin] = useState(false);
 	const [showResults, setShowResults] = useState(false);
 	const [prizeNumber, setPrizeNumber] = useState(0);
-	const location = useGeolocation();
+	const [selectedOption, setSelectedOption] = useState(null);
+	const [list, updateList] = useState(data);
 
+	const location = useGeolocation();
+	/**
+	 *  set geo location information
+	 */
 	if (location.loaded) {
 		latitude = location.coordinates.lat;
 		longitude = location.coordinates.lng;
 	}
 
+	/**
+	 * function handles the wheel spin
+	 * generates and sets a random prize number in which the wheel will end its spinning animation
+	 */
 	const handleSpinClick = () => {
 		const newPrizeNumber = Math.floor(Math.random() * data.length);
 		setPrizeNumber(newPrizeNumber);
@@ -36,6 +51,10 @@ const Roulette = () => {
 
 	finalCusine = data[prizeNumber].option;
 
+	/**
+	 *
+	 * @returns The results button with bath to results page
+	 */
 	const ResultsBtn = () => {
 		return (
 			<NavBtn style={{ marginLeft: "2rem" }}>
@@ -43,6 +62,33 @@ const Roulette = () => {
 			</NavBtn>
 		);
 	};
+
+	/**
+	 * handles the current selected cuisine type to be displayed in the select element
+	 * @param {*} e event
+	 */
+	const handleSelectChange = (e) => {
+		setSelectedOption(e);
+	};
+
+	/**
+	 * function gets the index of the selected cusine type to remove and searches for the index in the data array.
+	 * the updateList hook is then called with the splices array without the removed cuisine type
+	 */
+	const removeCuisine = () => {
+		if (typeof selectedOption === "object" && selectedOption !== null) {
+			const indexToRemove = data.findIndex((object) => {
+				return object.option === selectedOption.option;
+			});
+
+			updateList(data.splice(indexToRemove, 1));
+			console.log(list);
+		}
+	};
+
+	const addCusine = () => {};
+
+	// rendering
 	return (
 		<>
 			<div align="center">
@@ -73,12 +119,43 @@ const Roulette = () => {
 				</button>
 				<br />
 
-				<h2>
-					{!mustSpin
-						? data[prizeNumber].option
-						: "Spinnng the Wheel..."}
-				</h2>
-				
+				<div>
+					<h2>
+						{!mustSpin
+							? data[prizeNumber].option
+							: "Spinnng the Wheel..."}
+					</h2>
+				</div>
+
+				<div
+					style={{
+						width: "43%",
+						display: "flex",
+						justifyContent: "space-between",
+						alignItems: "center",
+					}}>
+					<label htmlFor="remove">
+						Remove cusine type from the wheel
+					</label>
+					<Select
+						placeholder="Select"
+						value={selectedOption}
+						options={data}
+						onChange={handleSelectChange}
+						name="remove"></Select>
+					<button onClick={removeCuisine}>Remove</button>
+				</div>
+				<div
+					style={{
+						width: "43%",
+						display: "flex",
+						justifyContent: "space-between",
+						alignItems: "center",
+					}}>
+					<label htmlFor="add">Add a cusine type to the wheel</label>
+					<input type="text" name="add"></input>
+					<button onClick={addCusine}>Add</button>
+				</div>
 				<br></br>
 				<br></br>
 				<div
